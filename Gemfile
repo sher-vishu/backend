ea4d6eaacff51a1e6d486f817679efeedfd17cac
Vrishti Shersia <vrishti.shersia@gmail.com>
2024-05-08 21:58:57 +0900

    updated database.yml

--- Gemfile
+++ Gemfile
@@ -70,3 +70,8 @@ group :test do
   gem "capybara"
   gem "selenium-webdriver"
 end
+
+group :production do
+  gem 'pg'
+end
+
--- lib/remove_duplicates.rb
+++
@@ -1,8 +0,0 @@
-# Find and display duplicate records for "Model 1" and "Model 2"
-duplicates = AiModel.where(name: ["Model 1", "Model 2"]).group(:name).having("count(*) > 1").pluck(:name)
-puts "Duplicate models: #{duplicates}"
-
-# Delete duplicate records for "Model 1" and "Model 2" except for the first occurrence
-AiModel.where(name: duplicates).group(:name).having("count(*) > 1").each do |duplicate|
-  AiModel.where(name: duplicate.name).offset(1).destroy_all
-end
